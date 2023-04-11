3313e670608f5ec26ef9eca245eb87e0d67360a8
huulai2510 <huulai2510@gmail.com>
2023-04-11 19:32:20 +0700

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import polyfillNode from "rollup-plugin-polyfill-node";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      plugins: [polyfillNode()],
+    },
+  },
   define: {
     global: {},
   },
-})
+  resolve: {
+    alias: {
+      "./runtimeConfig": "./runtimeConfig.browser",
+    },
+  },
+});
